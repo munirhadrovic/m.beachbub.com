c7eec47cc281792abebb52713eeb8d57ada3a990
Emir <emiratesbp@gmail.com>
2015-04-24 13:11:01 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,41 @@
+<?php
+	$adresa[0]="2359 Vanderbilt Beach Rd #406"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Naples, FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Florida";
+	$telephone[0]=2392732350;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="Airport Pulling Rd N"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Naples, FL 34109";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Outdoor Farmers Market";
+	$telephone[1]=2392732350;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="2477 Periwinkle Way"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Sanibel, FL 33957";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Bailey's General Store";
+	$telephone[2]=2394271516;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="3167 South Atlantic Ave"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Daytona Beach, FL 32118";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Pollywanna's Beach Eats";
+	$telephone[3]=2392732350;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="2914 Sandy Ridge Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Colfax, NC 27235";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="North Carolina";
+	$telephone[4]=3363930214;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="5901 South Kings Highway"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Myrtle Beach, SC 29575";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="South Carolina";
+	$telephone[5]=8432385161;// Broj koji se poziva na dugme CALL US
+	
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flow"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
